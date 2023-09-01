69560366524b8071d8cc455b5f3ae6d55be9a478
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-09-01 00:44:12 -0400

    final commit for the night

--- sandbox.py
+++ sandbox.py
@@ -11,12 +11,38 @@ G
 # some testers to cut and paste
 
 
-print(mango.add_amount_to_cart(2))
-print(mango.what_kind())
-print(mango.what_it_cost())
-print(mango.fridge())
+# print(mango.add_amount_to_cart(2))
+# print(mango.what_kind())
+# print(mango.what_it_cost())
+# print(mango.fridge())
 
-print(milk.add_amount_to_cart(2))
-print(milk.what_kind())
-print(milk.what_it_cost())
-print(milk.fridge())
+# print(milk.add_amount_to_cart(2))
+# print(milk.what_kind())
+# print(milk.what_it_cost())
+# print(milk.fridge())
+
+# Exercise 2 - Write a Python class which has two methods get_String and print_String. get_String accept a string from the user and print_String print the string in upper case¶
+
+"""
+two methods get_string  /       gets the string
+print_string. / print the string
+
+"""
+
+class Stringy:
+    def __init__(self):
+        self.repeat_string = " "
+                    
+    def get_string(self):
+        self.repeat_string = input("Enter whatever your heart's desire: ")
+        
+    def print_string(self):
+        print(self.repeat_string.upper())
+    
+stringy_instance = Stringy()
+
+stringy_instance.get.string()
+
+stringy_instance.print.string()
+
+  
